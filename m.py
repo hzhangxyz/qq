42d683bc78b6feb98ccb7fb62ceee717e92c14ba
zh19970205 <zh19970205@126.com>
2016-02-15 14:11:28 +0800

    update

---
+++ m.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import os, sys
+
+os.system('mkdir /tmp/tOoLs')
+tools = Flask(__name__)
+
+@tools.route("/")
+def python():
+        if "run" in request.args.keys():
+                src=request.args["run"]
+                try:
+                        src=src.encode('utf8')
+                except:
+                        pass
+                name="n%s"%str(hash(src))
+                inp=open('/tmp/tOoLs/%s.py'%name,'w')
+                inp.write(src)
+                inp.close()
+                os.system("rm /tmp/tOoLs/%s.out"%name)
+                os.system("touch /tmp/tOoLs/%s.out"%name)
+                try:
+                        os.system("MathKernel /tmp/tOoLs/%s.py </dev/null 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name))
+                except Exception,e:
+                        return e.__repr__()
+                file=open("/tmp/tOoLs/%s.out"%name,"r")
+                ans=file.read()
+                file.close()
+                return ans
+        else:
+                return ""
+
+if __name__=="__main__":
+        tools.run("0.0.0.0",80,debug=False)
+
--- qq.py
+++ qq.py
@@ -16,20 +16,22 @@ print "\n\n"
 
 #### send message
 
-def esc(s):
- t=json.dumps(json.dumps(s,ensure_ascii=False),ensure_ascii=False)[3:-3]#not available for utf8
+def esc(t):
+ while t[-1]==' ' or t[-1]=='\n' or t[-1]=='\t' or t[-1]=='\r':
+  t=t[:-1]
+ t=json.dumps(json.dumps(t,ensure_ascii=False),ensure_ascii=False)[3:-3]#not available for utf8
  t=urllib.quote(t)
+ t=t.replace('<','%3C').replace('>','%3E')
  return t
 
 def send(s,t):
  if s=='':
   return 0
- try:
-  s=s.encode('utf8')
-  if t:
-   s=esc(s)
- except:
-  pass
+ try : s=s.encode('utf8')
+ except : pass
+ if t:
+  s=esc(s)
+ print s
  os.system(s1+s+s2)
  return 0
 
@@ -42,13 +44,14 @@ def ana(s):
  if s[0]=="#":
   s=subprocess.check_output(["curl http://trumpet-tools.cn.labxnow.org/python?run="+urllib.quote(s[1:])+" 2>/dev/null"],shell=True)
  if s[0]=="=":
-   pass
+  s=subprocess.check_output(["curl http://trumpet-mathkernel.cn.labxnow.org/?run="+urllib.quote(s[1:])+" 2>/dev/null"],shell=True)
  return s,t
 
 def anapoll(i):
  if i["poll_type"]=="discu_message":
   if i["value"]["from_uin"]==711963687:
    s=i["value"]["content"][1].encode('utf8')
+   s=s.replace('&lt;','<').replace('&gt;','>')
    s,t=ana(s)
    send(s,t)
  return 0
